e58bebf6ceddf63aaa6672701b41fb8e3a5cdbc1
Jonas Wiesli <wiesli.jonas@gmail.com>
2023-02-03 23:16:43 +0100

    added basic crossroad nodes

---
+++ Assets/Prefabs/Crossroad/Crossroad.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crossroad : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> connectedCrossroads = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject crossroad in connectedCrossroads)
+        {
+            crossroad.GetComponent<Crossroad>().ensureConnectionBothWays(this.gameObject);
+        }
+    }
+
+    public void ensureConnectionBothWays(GameObject otherCrossroad)
+    {
+        if(!connectedCrossroads.Contains(otherCrossroad))
+        {
+            connectedCrossroads.Add(otherCrossroad);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
