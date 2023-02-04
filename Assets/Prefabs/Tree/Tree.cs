0800fe51574a149805085764fee5631d3d0d3948
Simon Hischier <simon_hischier@hotmail.com>
2023-02-04 16:41:40 +0100

    added hp and attack component

--- Assets/Prefabs/Tree/Tree.cs
+++ Assets/Prefabs/Tree/Tree.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Tree : MonoBehaviour
@@ -15,6 +13,7 @@ public class Tree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // TODO replace/remove hp and attack parts
         treeRenderer = GetComponent<Renderer>();
 
         currentHealth = startingHealth;
