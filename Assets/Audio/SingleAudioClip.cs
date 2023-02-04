b3dc83dd7a907163bb2a3f027a47e9c289853718
NeophyteCH <skulfur@tuta.io>
2023-02-04 19:05:51 +0100

    unified audio files and added SO to group them

---
+++ Assets/Audio/SingleAudioClip.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SingleAudioClip
+{
+    [SerializeField] private AudioClip audioClip;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float volume = 1f;
+
+
+    public AudioClip GetAudioClip { get => audioClip; }
+    public float GetVolume { get => volume; }
+   
+
+}
