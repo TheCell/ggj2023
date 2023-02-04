fe711bf1d7e4c38e3d0b5cb81f96fcbec4bb7249
Jonas Wiesli <wiesli.jonas@gmail.com>
2023-02-04 22:31:13 +0100

    implemented tap action workaround

--- Assets/Code/Global-Code/Input Action/TapAction.cs
+++ Assets/Code/Global-Code/Input Action/TapAction.cs
@@ -6,6 +6,7 @@ public class TapAction : MonoBehaviour
 {
     [SerializeField] private TapActionConfig tapActionConfig;
     private int healAmount;
+    private bool cooldown = false;
 
     void Start()
     {
@@ -14,27 +15,52 @@ public class TapAction : MonoBehaviour
 
     public void tapAction()
     {
-        if (GetComponent<Tree>())
+        if(!cooldown)
         {
-            this.GetComponent<Health>().Heal(healAmount);
-            this.GetComponent<Tree>().SetSize();
-        }
-        else if (GetComponent<Crossroad>())
-        {
-            if(this.GetComponent<Crossroad>().HasTree())
+            Debug.Log("Fired!");
+            if (GetComponent<Tree>())
             {
-                this.GetComponentInChildren<Health>().Heal(healAmount);
-                this.GetComponentInChildren<Tree>().SetSize();
-            } 
-            else
-            {
-                this.GetComponent<Crossroad>().PrepareTree();
+                this.GetComponent<Health>().Heal(healAmount);
+                this.GetComponent<Tree>().SetSize();
             }
+            else if (GetComponent<Crossroad>())
+            {
+                if (this.GetComponent<Crossroad>().HasTree())
+                {
+                    this.GetComponentInChildren<Health>().Heal(healAmount);
+                    this.GetComponentInChildren<Tree>().SetSize();
+                }
+                else
+                {
+                    this.GetComponent<Crossroad>().PrepareTree();
+                }
+            }
+
+            cooldown = true;
+            StartCoroutine(ResetCooldown());
         }
+    }
+
+    private IEnumerator ResetCooldown()
+    {
+        yield return new WaitForEndOfFrame();
+        cooldown = false;
     }
 
     private void ApplyConfig(TapActionConfig tapActionConfig)
     {
-        healAmount= tapActionConfig.healAmount;
+        healAmount = tapActionConfig.healAmount;
+    }
+
+    private float lasttime;
+
+    private void Update()
+    {
+        if (Time.time > lasttime + 2)
+        {
+            lasttime = Time.time;
+            //if(transform.GetSiblingIndex() % 2 == 0)
+            GameObject.FindGameObjectsWithTag("Crossroad")[5].GetComponent<TapAction>().tapAction();
+        }
     }
 }
